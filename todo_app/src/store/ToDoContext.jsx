18a00364a212f69aa9ed3de0a8fc01e04fd18d1c
CoolShivas <shivas2710cool00@gmail.com>
2024-05-12 19:48:17 +0530

    Getting the data from the localStorage after refreshing the page

--- todo_app/src/store/ToDoContext.jsx
+++ todo_app/src/store/ToDoContext.jsx
@@ -1,13 +1,32 @@
 import { createContext, useEffect, useState } from "react";
 
-const ToDoContext = createContext();
+const ToDoContext = createContext({
+    textInput: '',
+    itemsArray: [],
+    addition: () => { },
+    subtraction: () => { },
+    removeAll: () => { },
+});
+
+
+const gettingLocalStorage = () => {
+    // Getting the data after refreshing the page. So, to get data after refreshing the page we have to take the data back from the localStorage to render it again on the page after refreshing also.
+
+    let localData = localStorage.getItem("Task");
+    if (!localData) {
+        return [];
+    }
+    else {
+        return JSON.parse(localData);
+    }
+};
 
 
 export const ToDoCtxProvider = (props) => {
 
     const [textInput, setTextInput] = useState('');
 
-    const [itemsArray, setItemsArray] = useState([]);
+    const [itemsArray, setItemsArray] = useState(gettingLocalStorage());
 
 
     const handlerOnAddBtn = () => {
@@ -36,6 +55,8 @@ export const ToDoCtxProvider = (props) => {
         // For storing the data into the localStorage by setItem by passing the name i.e, "Task", and to store the array in the form of string. We have to use the JSON.stringify() to convert it into string. What we have to convert i.e, the array where we storing the data with the help of useState i.e, itemsArray. 
 
         // And we know that useEffect render only first time ( or render once with blank [] as dependency) if the data updates then we have to pass the dependencies inside the Array.
+
+        // But here is the problem after refreshing the page the data is lost from both i.e, localStorage and page too.
 
     }, [itemsArray]);
 
