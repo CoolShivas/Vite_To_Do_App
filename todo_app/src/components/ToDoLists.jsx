7fa6bb62103361952c591ddefa3dddcefd528fb9
CoolShivas <shivas2710cool00@gmail.com>
2024-05-12 14:24:57 +0530

    Implementation on Delete button

--- todo_app/src/components/ToDoLists.jsx
+++ todo_app/src/components/ToDoLists.jsx
@@ -2,10 +2,14 @@ import { IoCloseCircle } from "react-icons/io5";
 
 const ToDoLists = ({ arrABC }) => {
 
+    const handlerOnCrossIcon = () => {
+        console.log('cross btn clicked');
+    };
+
     return (
         <div className="todo_style">
             <li>
-                <IoCloseCircle className="cross_icon" />
+                <IoCloseCircle className="cross_icon" onClick={handlerOnCrossIcon} />
                 {arrABC}
             </li>
         </div>
