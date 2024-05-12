1e872eb2b87028cbf04aac5aa078dc2b8b08b3f2
CoolShivas <shivas2710cool00@gmail.com>
2024-05-12 12:03:34 +0530

    CSS added

--- todo_app/src/App.jsx
+++ todo_app/src/App.jsx
@@ -3,7 +3,19 @@
 const App = () => {
   return (
     <>
-      <h1> To Do App </h1>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1> To Do App </h1>
+          <br />
+          <input type="text" placeholder="add an item here" />
+          <button> + </button>
+          <ol>
+            <li> buy apple </li>
+          </ol>
+        </div>
+      </div>
+
     </>
   )
 }
